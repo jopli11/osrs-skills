8aec1c7f0de6128596c5661c5a22480224460161
jopli11 <47662793+jopli11@users.noreply.github.com>
2025-07-20 14:28:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from 'next/script';
 import { Providers } from "@/app/providers";
 import "./globals.css";
 import "./styles.css";
@@ -163,19 +164,6 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://oldschool.runescape.wiki" />
         <link rel="preconnect" href="https://oldschool.runescape.wiki" crossOrigin="anonymous" />
         
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NNZPR8WP');
-            `
-          }}
-        />
-        
         <script dangerouslySetInnerHTML={{
           __html: `
             // Simple client-side performance tracking
@@ -204,6 +192,21 @@ export default function RootLayout({
         }} />
       </head>
       <body className="osrs-background" suppressHydrationWarning>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NNZPR8WP');
+            `
+          }}
+        />
+        
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
